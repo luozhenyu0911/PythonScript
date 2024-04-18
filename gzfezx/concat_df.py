16f503e6a85f4dcc3be5dd992e1c0d29eb41829d
luozhenyu0911 <1018954087@qq.com>
2024-04-18 15:02:21 +0800

    2014.04.18 updated

--- gzfezx/concat_df.py
+++ gzfezx/concat_df.py
@@ -10,21 +10,25 @@ parser = argparse.ArgumentParser(description="The script is to .",
                                 epilog=example_text)
 
 parser.add_argument('--glob_xx','-g',type=str,help="",required= True,metavar='')
+parser.add_argument('--columns', '-c',type= int,help="how many columns to keep",required= True,metavar='')
 parser.add_argument('--prefix', '-p',type= str,help="",required= True,metavar='')
 args = parser.parse_args()
 
 glob_xx = args.glob_xx
 prefix = args.prefix
+columns = args.columns
 
 file_list = glob.glob("*"+glob_xx)
 # print(len(argv))
 # print(argv[4])
 # 创建一个空的DataFrame作为初始值
 result_df = pd.DataFrame()
-
+num=[]
+for i in range(int(columns)):
+    num.append(i)
 # 循环对多个文件两两合并
 for filename in file_list:
-    df1 = pd.read_csv(filename, sep='\t', header=None)
+    df1 = pd.read_csv(filename, sep='\t', header=None, usecols=num)
     name = filename.split('.')[0]
     df1['sample'] = name
     df1.columns = ['region', 'count', 'sample']
